dd7dfa0554115e52df6313f823dafcb04277e9b7
Leo <lpolicastro@pm.me>
2020-11-16 14:23:01 -0500

    add get request to fetch all gists

--- GitHub.py
+++ GitHub.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, redirect, url_for, request
 import requests
 import json
 import os
+import numpy as np
 
 github = Blueprint("github", __name__)
 
@@ -16,15 +17,32 @@ print("Request URL: %s" % url)
 headers = {"Authorization": "token %s" % API_TOKEN}
 params = {"scope": "gist"}
 payload = {
-        "description": "my gists",
-        "public": True,
-    }
+    "description": "my gists",
+    "public": True,
+}
 
 
-@github.route("/github/", methods=["POST"])
+@github.route("/github/", methods=["POST", "GET"])
 def git():
-    result = json.loads(request.data.decode("utf-8"))
-    payload.update(result)
-    res = requests.post(url, headers=headers, params=params, data=json.dumps(payload))
-    j = json.loads(res.text)
-    return jsonify({"id": j["id"]})
+    if request.method == "POST":
+        result = json.loads(request.data.decode("utf-8"))
+        payload.update(result)
+        res = requests.post(
+            url, headers=headers, params=params, data=json.dumps(payload)
+        )
+        j = json.loads(res.text)
+        return jsonify({"id": j["id"]})
+    else:
+        run = True
+        gist_array = []
+        page = 1
+        while run:
+            data = requests.get(
+                f"{url}?page={page}&per_page=100", headers=headers, params=params
+            )
+            result = json.loads(data.content.decode("utf-8"))
+            page += 1
+            gist_array.extend(result)
+            if len(result) < 100:
+                run = False
+        return jsonify(gist_array)
